b1377bd41e4fe40f3a61a397ee413c8b61c373d6
Loureiro <mindera@C02Z689TLVCF-ME00547.local>
2025-04-02 16:18:54 +0100

    Add First Step in security to create userLogin and userRegister

--- src/main/java/com/mindera/school/security/SecurityConfig.java
+++ src/main/java/com/mindera/school/security/SecurityConfig.java
@@ -12,8 +12,22 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable());
+               http.authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/api/v1/login", "/api/v1/register").permitAll()
+                        .requestMatchers("/api/v1/teacher/**").hasRole("TEACHER")
+                        .requestMatchers("/api/v1/student/**").hasRole("STUDENT")
+                        .requestMatchers("/api/v1/dev/**").hasRole("DEV")
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/api/v1/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
         return http.build();
     }
+
+
 
 }
