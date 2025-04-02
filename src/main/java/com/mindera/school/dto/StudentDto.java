b1cdd0b3ab556c9aa8ef6542bb446c263f2b11bd
Loureiro <mindera@C02Z689TLVCF-ME00547.local>
2025-04-02 11:08:05 +0100

    HotFix StudentDTO

--- src/main/java/com/mindera/school/dto/StudentDto.java
+++ src/main/java/com/mindera/school/dto/StudentDto.java
@@ -1,4 +1,20 @@
 package com.mindera.school.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class StudentDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private Date birthdate;
+    private String email;
 }
