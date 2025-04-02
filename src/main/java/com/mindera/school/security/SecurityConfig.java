58194b3c5c59b5e903af0c5b1f5ff34a03926f6a
Loureiro <mindera@C02Z689TLVCF-ME00547.local>
2025-04-02 16:51:55 +0100

    Hot Fix Security to work with Swagger Dashboard

--- src/main/java/com/mindera/school/security/SecurityConfig.java
+++ src/main/java/com/mindera/school/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.mindera.school.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,23 +12,18 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-               http.authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/api/v1/login", "/api/v1/register").permitAll()
-                        .requestMatchers("/api/v1/teacher/**").hasRole("TEACHER")
-                        .requestMatchers("/api/v1/student/**").hasRole("STUDENT")
-                        .requestMatchers("/api/v1/dev/**").hasRole("DEV")
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v1/login", "/api/v1/register", "/swagger-ui/**", "/api-docs", "/swagger-ui/index.html", "/v2/api-docs").permitAll()
+                        .requestMatchers("/api/v1/teacher/**").hasAuthority("TEACHER")
+                        .requestMatchers("/api/v1/student/**").hasAuthority("STUDENT")
+                        .requestMatchers("/api/v1/dev/**").hasAuthority("DEV")
                         .anyRequest().authenticated()
                 )
-                .formLogin((form) -> form
-                        .loginPage("/api/v1/login")
-                        .permitAll()
-                )
-                .logout((logout) -> logout.permitAll());
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
-
-
-
 }
